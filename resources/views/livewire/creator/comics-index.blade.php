5dc81f08264d208b6b95e817381aeebaa1b7a874
PabloCoz <pablocr_234@hotmail.com>
2022-12-18 15:35:55 -0500

    actual

--- resources/views/livewire/creator/comics-index.blade.php
+++ resources/views/livewire/creator/comics-index.blade.php
@@ -65,44 +65,49 @@
                                             <div class="text-sm text-gray-900 flex items-center">
                                                 <div class="flex items-center">
                                                     <ul class="flex text-sm">
-                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                                stroke-width="1.5" stroke="currentColor"
+                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor"
                                                                 class="w-6 h-6 text-{{ $value >= 1 ? 'yellow' : 'gray' }}-500">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                                                             </svg>
                                                         </li>
-                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                                stroke-width="1.5" stroke="currentColor"
+                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor"
                                                                 class="w-6 h-6 text-{{ $value >= 2 ? 'yellow' : 'gray' }}-500">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                                                             </svg>
                                                         </li>
-                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                                stroke-width="1.5" stroke="currentColor"
+                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor"
                                                                 class="w-6 h-6 text-{{ $value >= 3 ? 'yellow' : 'gray' }}-500">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                                                             </svg>
                                                         </li>
-                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                                stroke-width="1.5" stroke="currentColor"
+                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor"
                                                                 class="w-6 h-6 text-{{ $value >= 4 ? 'yellow' : 'gray' }}-500">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                                                             </svg>
                                                         </li>
-                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                                stroke-width="1.5" stroke="currentColor"
+                                                        <li class="mr-1"><svg xmlns="http://www.w3.org/2000/svg"
+                                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor"
                                                                 class="w-6 h-6 text-{{ $value >= 5 ? 'yellow' : 'gray' }}-500">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                                                             </svg>
                                                         </li>
-                                        
+
                                                     </ul>
-                                        
+
                                                 </div>
                                             </div>
                                             <div class="text-sm text-gray-500">{{ $comic->rating }}</div>
